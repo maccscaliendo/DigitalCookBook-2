1ee91d082bea4015e75a764de6fd9879888c9930
maccscaliendo <maccscaliendo@hotmail.com>
2016-12-02 11:13:17 +0000

    Day One Development
    Adding the User Interface

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,56 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* listitem  Pancakes */
+    $(document).on("click", ".uib_w_4", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#uib_page_pancakes"); 
+         return false;
+    });
+    
+        /* button  #backBtnPancakes */
+    $(document).on("click", "#backBtnPancakes", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+     
+     
+     
+     var iabRef = null;
+
+    // Inject our custom JavaScript into the InAppBrowser window
+    //
+    
+
+    function iabClose(event) {
+         iabRef.removeEventListener('exit', iabClose);
+    }
+     
+     
+     
+     
+     
+        /* button  #takeAwayBtn */
+    $(document).on("click", "#takeAwayBtn", function(evt)
+    {
+         iabRef = window.open('http://www.shawacademy.com', '_blank', 'location=yes');
+         
+         iabRef.addEventListener('exit', iabClose);
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
